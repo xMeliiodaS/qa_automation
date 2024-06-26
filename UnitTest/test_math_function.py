7810d12fd6e71857674381c88b96524bbebdc165
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-06-26 09:36:53 +0300

    Added some comments

--- UnitTest/test_math_function.py
+++ UnitTest/test_math_function.py
@@ -7,10 +7,15 @@ from math_function import modulo_function
 class TestMathFunction(unittest.TestCase):
 
     def test_is_even_true(self):
+        # Arrange
         num = 4  # Mock
+
+        # Act
         result = is_even(num)
+
+        # Assert
         self.assertTrue(result)
 
     def test_modulo_function_zero(self):
         self.assertEqual(0, modulo_function(4), "The function not working well.")
-        # Expected, Actual
+        # self.assertEqual(Expected, Actual, msg)
