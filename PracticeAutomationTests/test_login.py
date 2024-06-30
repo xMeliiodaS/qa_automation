25f3229a612ef319258559a9e02c9554bb34f8c2
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-06-30 23:05:35 +0300

    Added new file to test the login nad created a incorrect login test

--- PracticeAutomationTests/test_login.py
+++ PracticeAutomationTests/test_login.py
@@ -20,3 +20,25 @@ def test_login_valid():
 
     assert "Hello" in driver.page_source
     driver.quit()
+
+
+def test_login_incorrect_username_password():
+    """
+    Test case for incorrect username and invalid password.
+    """
+    driver = webdriver.Chrome()
+
+    driver.get("https://practice.automationtesting.in/my-account/")
+    time.sleep(1)
+
+    username = "Bahaa.abozlf5753g"
+    driver.find_element(By.ID, "username").send_keys(username)
+    driver.find_element(By.ID, "password").send_keys("HiiGuys912erg3")
+
+    driver.find_element(By.NAME, "login").click()
+    time.sleep(1)
+
+    assert f"The username <strong>{username}</strong> is not registered on this site." \
+           in driver.page_source, "Expected error message not found"
+
+    driver.quit()
