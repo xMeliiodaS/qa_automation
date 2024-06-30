63070cc4a1869dd086682aac374041927dc5a06b
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-06-30 21:37:59 +0300

    Added 2 more function one for empty email address and the second is for empty password

--- PracticeAutomationTests/test_registration.py
+++ PracticeAutomationTests/test_registration.py
@@ -46,3 +46,43 @@ def test_register_invalid_email():
     assert failure_message in driver.page_source, "Invalid email error message not found"
 
     driver.quit()
+
+
+def test_register_empty_email():
+    driver = webdriver.Chrome()
+    driver.get("https://practice.automationtesting.in/my-account/")
+    #   driver.find_element(By.LINK_TEXT, "My Account").click()
+
+    time.sleep(1)
+
+    driver.find_element(By.ID, "reg_email").send_keys("")
+    driver.find_element(By.ID, "reg_password").send_keys("HiiGuys912")
+
+    register_button = driver.find_element(By.CSS_SELECTOR,
+                                          "input.woocommerce-Button[name='register'][value='Register']")
+
+    register_button.click()
+    time.sleep(2)
+
+    failure_message = " Please provide a valid email address."
+    assert failure_message in driver.page_source, "Empty email error message not found"
+
+
+def test_register_empty_password():
+    driver = webdriver.Chrome()
+    driver.get("https://practice.automationtesting.in/my-account/")
+    #   driver.find_element(By.LINK_TEXT, "My Account").click()
+
+    time.sleep(1)
+
+    driver.find_element(By.ID, "reg_email").send_keys("legof60747@devncie.com")
+    driver.find_element(By.ID, "reg_password").send_keys("")
+
+    register_button = driver.find_element(By.CSS_SELECTOR,
+                                          "input.woocommerce-Button[name='register'][value='Register']")
+
+    register_button.click()
+    time.sleep(2)
+
+    failure_message = " Please enter an account password."
+    assert failure_message in driver.page_source, "Empty password error message not found"
