06d27c723aef549614066f4ea59f178d91dcbbc9
xMeliiodaS <bahaa.abozlf57@gmail.com>
2024-07-03 11:34:39 +0300

    Practice the POM

--- pom/practice/logic/login_page.py
+++ pom/practice/logic/login_page.py
@@ -11,4 +11,21 @@ class LoginPage(BasePage):
         super().__init__(driver)
         self._user_name_input = self._driver.find_element(By.XPATH, self.USER_NAME_INPUT)
         self._password_input = self._driver.find_element(By.XPATH, self.PASSWORD_INPUT)
-        self._submit_input = self._driver.find_element(By.XPATH, self.LOGIN_BUTTON)
+        self._login_button = self._driver.find_element(By.XPATH, self.LOGIN_BUTTON)
+
+    def fill_user_name_input(self, username):
+        self._user_name_input.clear()
+        self._user_name_input.send_keys(username)
+
+    def fill_password_input(self, password):
+        self._password_input.clear()
+        self._password_input.send_keys(password)
+
+    def click_submit_button(self):
+        self._login_button.click()
+
+    def login_flow(self, username, password):
+        self.fill_user_name_input(username)
+        self.fill_password_input(password)
+        self.click_submit_button()
+
