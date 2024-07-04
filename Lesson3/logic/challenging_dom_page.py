294cdcac1ab5f9714187c51c75a9186044178239
NOTxMeliiodaaS <kotarou.gaia557@gmail.com>
2024-07-04 17:11:11 +0300

    challenging dom

--- Lesson3/logic/challenging_dom_page.py
+++ Lesson3/logic/challenging_dom_page.py
@@ -7,17 +7,27 @@ class ChallengingDomPage(BasePage):
     SECOND_BUTTON = '//a[@class="button alert"]'
     THIRD_BUTTON = '//a[@class="button success"]'
 
+    ROWS_IN_TABLE = '//tr'
+    EDIT_ROW = '//tr//td//a[text() = "edit"]'
+
     def __init__(self, driver):
         super().__init__(driver)
         self._first_button = self._driver.find_element(By.XPATH, self.FIRST_BUTTON)
         self._second_button = self._driver.find_element(By.XPATH, self.SECOND_BUTTON)
         self._third_button = self._driver.find_element(By.XPATH, self.THIRD_BUTTON)
+        self._rows_in_table = self._driver.find_elements(By.XPATH, self.ROWS_IN_TABLE)
+        self._edit_row = self._driver.find_elements(By.XPATH, self.EDIT_ROW)
 
     def click_on_first_button(self):
         self._first_button.click()
 
-    def click_on_first_button(self):
-        self._first_button.click()
+    def click_on_second_button(self):
+        self._second_button.click()
 
-    def click_on_first_button(self):
-        self._first_button.click()
+    def click_on_third_button(self):
+        self._third_button.click()
+
+    def click_on_edit(self, index):
+        self._edit_row[index].click()
+
+
--- Lesson3/test/test.py
+++ Lesson3/test/test.py
@@ -2,6 +2,7 @@ import time
 import unittest
 from infra.browser_wrapper import BrowserWrapper
 from logic.add_remove_elements_page import AddRemoveElementsPage
+from logic.challenging_dom_page import ChallengingDomPage
 from logic.home_page import HomePage
 
 
@@ -22,3 +23,16 @@ class Test(unittest.TestCase):
 
         add_remove_elements.click_on_delete(3)
         time.sleep(1)
+
+    def test_add_remove_asdelement(self):
+        driver = BrowserWrapper().get_driver(self.config["base_url"])
+        time.sleep(0.1)
+        home_page = HomePage(driver)
+        home_page.click_on_challenging_dom_link()
+
+        time.sleep(0.5)
+        cas = ChallengingDomPage(driver)
+        cas.click_on_edit(5)
+        time.sleep(0.5)
+
+        time.sleep(153)
