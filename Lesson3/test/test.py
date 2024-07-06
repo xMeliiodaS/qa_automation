f298718b6016e7a419da70ccfe82b023f3bd4f84
NOTxMeliiodaaS <kotarou.gaia557@gmail.com>
2024-07-06 21:46:52 +0300

    Modified the config usage

--- Lesson3/test/test.py
+++ Lesson3/test/test.py
@@ -13,124 +13,115 @@ from logic.home_page import HomePage
 
 
 class Test(unittest.TestCase):
-    browser = BrowserWrapper()
-    config = browser.config
+    config = BrowserWrapper().config
+    driver = BrowserWrapper().get_driver(config["base_url"])
 
     def test_add_remove_element(self):
-        driver = BrowserWrapper().get_driver(self.config["base_url"])
         time.sleep(0.1)
-        home_page = HomePage(driver)
+        home_page = HomePage(self.driver)
         home_page.click_on_add_remove_link()
 
         time.sleep(0.5)
-        add_remove_elements = AddRemoveElementsPage(driver)
+        add_remove_elements = AddRemoveElementsPage(self.driver)
         add_remove_elements.click_on_add_elements_counts(5)
         time.sleep(0.5)
 
         add_remove_elements.click_on_delete(3)
         time.sleep(1)
-        driver.quit()
+        self.driver.quit()
 
     def test_challenging_dom(self):
-        driver = BrowserWrapper().get_driver(self.config["base_url"])
         time.sleep(0.1)
-        home_page = HomePage(driver)
+        home_page = HomePage(self.driver)
         home_page.click_on_challenging_dom_link()
 
         time.sleep(0.5)
-        cas = ChallengingDomPage(driver)
+        cas = ChallengingDomPage(self.driver)
         cas.click_on_edit(5)
         time.sleep(0.5)
-        driver.quit()
+        self.driver.quit()
 
-    def test_challenging_dom(self):
-        driver = BrowserWrapper().get_driver(self.config["base_url"])
+    def test_challenging_dom_a(self):
         time.sleep(0.1)
-        home_page = HomePage(driver)
+        home_page = HomePage(self.driver)
         home_page.click_on_checkboxes_link()
 
         time.sleep(0.5)
-        cb = CheckBoxesPage(driver)
+        cb = CheckBoxesPage(self.driver)
         cb.click_on_all_checkboxes()
-        driver.quit()
+        self.driver.quit()
 
     def test_context_menu(self):
-        driver = BrowserWrapper().get_driver(self.config["base_url"])
         time.sleep(0.1)
-        home_page = HomePage(driver)
+        home_page = HomePage(self.driver)
         home_page.click_on_context_menu_link()
 
         time.sleep(0.5)
-        cmp = ContextMenuPage(driver)
+        cmp = ContextMenuPage(self.driver)
         cmp.right_click_on_context_menu()
-        driver.quit()
+        self.driver.quit()
 
     def test_drag_and_drop_items(self):
-        driver = BrowserWrapper().get_driver(self.config["base_url"])
         time.sleep(0.1)
-        home_page = HomePage(driver)
+        home_page = HomePage(self.driver)
         home_page.click_on_drag_and_drop_link()
 
         time.sleep(0.5)
-        drag_drop = DragAndDrop(driver)
+        drag_drop = DragAndDrop(self.driver)
         drag_drop.drag_first_item_to_second_item()
         time.sleep(4)
         drag_drop.drag_second_item_to_first_item()
-        driver.quit()
+        self.driver.quit()
 
     def test_select_dropdown(self):
-        driver = BrowserWrapper().get_driver(self.config["base_url"])
         time.sleep(0.1)
-        home_page = HomePage(driver)
+        home_page = HomePage(self.driver)
         home_page.click_on_dropdown_link()
 
         time.sleep(0.5)
-        dp = DropdownPage(driver)
+        dp = DropdownPage(self.driver)
         dp.select_dropdown_by_value()
         time.sleep(4)
         dp.select_dropdown_by_index(2)
-        driver.quit()
+        self.driver.quit()
 
     def test_dynamic_content_text(self):
-        driver = BrowserWrapper().get_driver(self.config["base_url"])
         time.sleep(0.1)
-        home_page = HomePage(driver)
+        home_page = HomePage(self.driver)
         home_page.click_on_dynamic_content_link()
 
         time.sleep(0.5)
-        dcp = DynamicContentPage(driver)
+        dcp = DynamicContentPage(self.driver)
 
         time.sleep(1)
         print(dcp.get_dynamic_content_text())
         dcp.click_on_here_button()
         time.sleep(1)
         print(dcp.get_dynamic_content_text())
-        driver.quit()
+        self.driver.quit()
 
     def test_dynamic_content_image(self):
-        driver = BrowserWrapper().get_driver(self.config["base_url"])
         time.sleep(0.1)
-        home_page = HomePage(driver)
+        home_page = HomePage(self.driver)
         home_page.click_on_dynamic_content_link()
 
         time.sleep(0.5)
-        dcp = DynamicContentPage(driver)
+        dcp = DynamicContentPage(self.driver)
 
         time.sleep(1)
         print(dcp.get_image_src())
         dcp.click_on_here_button()
         time.sleep(1)
         print(dcp.get_image_src())
-        driver.quit()
+        self.driver.quit()
 
-    def test_dynamic_controls(self):
-        driver = BrowserWrapper().get_driver(self.config["base_url"])
+    def test_dynamic_controls_checkbox(self):
         time.sleep(0.1)
-        home_page = HomePage(driver)
+        home_page = HomePage(self.driver)
         home_page.click_on_dynamic_controls_link()
 
         time.sleep(0.5)
-        dcp = DynamicControlsPage(driver)
+        dcp = DynamicControlsPage(self.driver)
 
         time.sleep(1)
         dcp.click_on_checkbox()
@@ -139,4 +130,22 @@ class Test(unittest.TestCase):
         time.sleep(1)
         dcp.click_on_checkbox_forced()
         time.sleep(5)
+        self.driver.quit()
+
+    def test_dynamic_controls_input(self):
+        time.sleep(0.1)
+        home_page = HomePage(self.driver)
+        home_page.click_on_dynamic_controls_link()
+
+        time.sleep(0.5)
+        dcp = DynamicControlsPage(self.driver)
+
+        time.sleep(1)
+        dcp.click_on_checkbox()
+        time.sleep(1)
+        dcp.click_on_remove_button()
+        time.sleep(1)
+        dcp.click_on_checkbox_forced()
+        time.sleep(5)
+        self.driver.quit()
 
